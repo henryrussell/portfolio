e9b175301631ea00c9e929701e7a0f45cc7d2a3d
Henry Russell <henryrussell@Henrys-MacBook-Pro.local>
2025-02-17 17:58:38 +0000

    small css improvements

--- my-app/src/app/components/Intro.tsx
+++ my-app/src/app/components/Intro.tsx
@@ -3,7 +3,7 @@ export default function Intro(){
     return(
         <div className="intro" data-testid='intro'>
             <h1> The Self Testing Website </h1>
-            <p> I created this site to showcase both my front end development and automated testing skillset. It includes various features like old projects, an animated background, light/dark mode, and a responsive section for rating my skills, but these are really placeholders for the main purpose - a website that runs tests on itself!</p>
+            <p> I created this site to showcase both my front end development and automated testing skillset. The site includes various features like old projects, an animated background, light/dark mode, and a responsive section for rating my skills, but these are really placeholders for the main purpose - a website that runs tests on itself!</p>
         </div>
     )
 }
