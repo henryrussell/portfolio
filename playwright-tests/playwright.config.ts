a28f7b32a664441bc99cca4f7af51ba66b1cd70d
Henry Russell <henryrussell@Henrys-MacBook-Pro.local>
2025-02-20 20:48:45 +0000

    set timeout for playwright

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   timeout: 60_000,
+  
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
